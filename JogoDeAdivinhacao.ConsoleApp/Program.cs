1defd35090a0e7297df3c812cbb5b093fa78ef65
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-09 15:29:49 -0300

    Adicionada geração de número aleatório

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -1,15 +1,23 @@
 ﻿// Objetivos / Passo-a-passo
 // 1. Nosso jogo deve aceitar o input do jogador e exibir o valor digitado
-// 2. Nosso jogo deve gerar um número secreto aleatório
+// 2. Nosso jogo deve gerar um número aleatório
 // 3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem
+
+using System;
+
+// Eu quero usar a biblioteca padrão do sistema relacionada a criptografia
+using System.Security.Cryptography;
 
 Console.WriteLine("------------------------------------");
 Console.WriteLine("Jogo de Adivinhação");
 Console.WriteLine("------------------------------------");
 
+int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+
 Console.Write("Digite um número entre 1 e 20: ");
 string? chute = Console.ReadLine();
 
 Console.WriteLine("O valor digitado foi: " + chute);
+Console.WriteLine("O valor aleatório era: " + numeroAleatorio);
 
 Console.ReadLine();
