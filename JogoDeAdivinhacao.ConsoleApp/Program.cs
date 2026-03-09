a55db8fdba0f5e7af6475b90f7682a393a8aff35
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-09 16:00:57 -0300

    Adicionada validação de input e loop de jogo

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -8,16 +8,47 @@ using System;
 // Eu quero usar a biblioteca padrão do sistema relacionada a criptografia
 using System.Security.Cryptography;
 
-Console.WriteLine("------------------------------------");
-Console.WriteLine("Jogo de Adivinhação");
-Console.WriteLine("------------------------------------");
+while (true == true)
+{
+    Console.Clear();
 
-int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+    Console.WriteLine("------------------------------------");
+    Console.WriteLine("Jogo de Adivinhação");
+    Console.WriteLine("------------------------------------");
 
-Console.Write("Digite um número entre 1 e 20: ");
-string? chute = Console.ReadLine();
+    int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
-Console.WriteLine("O valor digitado foi: " + chute);
-Console.WriteLine("O valor aleatório era: " + numeroAleatorio);
+    Console.Write("Digite um número entre 1 e 20: ");
+    string? chute = Console.ReadLine();
 
-Console.ReadLine();
+    int numeroDigitado = Convert.ToInt32(chute);
+
+    if (numeroDigitado == numeroAleatorio)
+    {
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("Parabéns, você acertou!");
+        Console.WriteLine("------------------------------------");
+    }
+    else if (numeroDigitado > numeroAleatorio)
+    {
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("O número digitado foi maior que o número secreto!");
+        Console.WriteLine("------------------------------------");
+    }
+    else
+    {
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("O número digitado foi menor que o número secreto!");
+        Console.WriteLine("------------------------------------");
+    }
+
+    Console.Write("Deseja continuar? (s/N): ");
+    string? opcaoContinuar = Console.ReadLine();
+
+    if (opcaoContinuar.ToUpper() != "S")
+    {
+        break;
+    }
+
+    Console.ReadLine();
+}
